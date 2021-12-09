097e6c8fad7e4fa2ec930c82441dab0781f01e3a
dorsa.kamali <dorsakamali779@gmail.com>
2021-12-09 22:43:06 +0330

    Assignment7

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function switchMode() {
     var mode = document.body;
     mode.classList.toggle("dark-mode");
 }
+
+$(document).ready(function () {
+    $("#showHide").click(function () {
+        $("p").toggle();
+    });
+
+});
+
+
